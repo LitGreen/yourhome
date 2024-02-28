8950f03bb53a12b47892ab9ab47d89f30f39762c
LitGreen <radostta4@gmail.com>
2024-02-28 14:46:21 +0000

    Multiselect filter fields and view update

--- yourhome/forms.py
+++ yourhome/forms.py
@@ -1,11 +1,23 @@
-
-from django.forms import ModelForm
 from .models import Property
 from django import forms
-from django.core import validators
 
 
-class MultiselectFilterForm(ModelForm):
-    class Meta:
-        model = Property
-        fields = '__all__'
+
+# class MultiselectFilterForm(forms.Form):
+
+#     class Meta:
+#         model = Property
+#         fields = ('advert_type', 'property_type', 'price_gt', 'price_lt')
+
+class MultiselectFilterForm(forms.Form):
+    advert_type = forms.ChoiceField(choices=Property.AdvertType.choices, required=False, widget=forms.Select(attrs={'class': 'select'}))
+    property_type = forms.MultipleChoiceField(choices=Property.PropertyType.choices, required=False, widget=forms.Select(attrs={'class': 'select'}))
+    price_gt = forms.IntegerField(required=False)
+    price_lt = forms.IntegerField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['advert_type'].widget.attrs['class'] = 'select'
+        self.fields['property_type'].widget.attrs['class'] = 'select'
+        self.fields['price_gt'].widget.attrs['class'] = 'form-control'
+        self.fields['price_lt'].widget.attrs['class'] = 'form-control'
