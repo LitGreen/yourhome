61b2696e14ecfc1e3f9f79ae03ab63e8ad1d9a3d
LitGreen <radostta4@gmail.com>
2024-02-29 18:20:04 +0000

    Filtered  properties template, navigation and view

--- yourhome/urls.py
+++ yourhome/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('multiselect_filter/', views.multiselectFilter, name="multiselect_filter"),
-]
+    path('filtered_properties/<str:property_type_slug>/<str:advert_type_slug>/', views.multiselectFilter, name="filtered_properties"),
+    ]
