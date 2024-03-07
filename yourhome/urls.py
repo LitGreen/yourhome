a4f3476ce064bee096d63ad2c089b5958e834246
LitGreen <radostta4@gmail.com>
2024-03-07 20:50:51 +0000

    Property model and templates update with new filter options

--- yourhome/urls.py
+++ yourhome/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('multiselect_filter/', views.multiselectFilter, name="multiselect_filter"),
-    path('filtered_properties/<str:property_type_slug>/<str:advert_type_slug>/', views.multiselectFilter, name="filtered_properties"),
+    path('filtered_properties/<str:property_type_slug>/<str:advert_type_slug>', views.multiselectFilter, name='filtered_properties'),
+    
     ]
